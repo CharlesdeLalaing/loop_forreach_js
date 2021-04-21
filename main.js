d8efc460db1c9e00c4ab62bf26264b314792f3dd
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-04-21 14:35:45 +0200

    oush

---
+++ main.js
@@ -0,0 +1,15 @@
+// -  # Exo 1 Boucles FOREACH
+//     ## - Creer une varaible de type array et inserez toutes les personnes de la classe
+//     ## - A l'aide d'une boucle foreach, dites bonjour + prenom à chaque personne du tableau
+
+let array1 = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
+
+array1.forEach(element => console.log("bonjour " + element));
+
+// - # Exo 2 Boucle FOREACH
+//     - ## Créez un un array qui contient 5 éléments
+//     - ## Affichez touts les éléments a l'aide d'une boucle foreach
+
+let array2 = ["une", "deux", "trois", "quattre", "cinq"];
+
+array2.forEach(element => console.log(element));
