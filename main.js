e1b8e721e77f8977b3210e2d404a747821f55bf3
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-04-21 15:07:12 +0200

    push

--- main.js
+++ main.js
@@ -4,7 +4,13 @@
 
 let array1 = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
 
-array1.forEach(element => console.log("bonjour " + element));
+//first method
+//array1.forEach(element => console.log("bonjour " + element));
+
+//second method
+// array1.forEach(e => {
+//     console.log("bonjour " + e)
+// })
 
 // - # Exo 2 Boucle FOREACH
 //     - ## Créez un un array qui contient 5 éléments
@@ -12,4 +18,10 @@ array1.forEach(element => console.log("bonjour " + element));
 
 let array2 = ["une", "deux", "trois", "quattre", "cinq"];
 
-array2.forEach(element => console.log(element));
+//first method
+// array2.forEach(element => console.log(element));
+
+//first method
+// array2.forEach(e => {
+//     console.log(e)
+// });
